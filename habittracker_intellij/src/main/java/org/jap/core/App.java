f9b4c639909be5f99fec88e4f9245ccd8d4b888a
Peter <pg055@hdm-stuttgart.de>
2021-11-01 16:38:09 +0100

    Done Better Things

--- habittracker_intellij/src/main/java/org/jap/core/App.java
+++ habittracker_intellij/src/main/java/org/jap/core/App.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.jap.ui.IUi;
 import org.jap.ui.UiFactory;
 import org.jap.ui.UiType;
+import org.jap.ui.console.ConsoleManager;
 
 public class App
 {
@@ -12,30 +13,59 @@ public class App
     
     private static IUi ui;
     
+    private static boolean loop = true;
+    
     public static void main(String[] args)
+    {
+        init(args);
+        
+        while(loop)
+        {
+            input();
+            processing();
+            output();
+        }
+    }
+    
+    private static void init(String[] args)
     {
         logger.debug("Debug");
         logger.info("Info");
         logger.warn("Warn");
         logger.error("Error");
         
+        UiType uiType = UiType.console;
         for (String arg : args)
         {
             for (UiType type : UiType.values())
-            {
                 if (arg.equals(type.toString()))
-                {
-                    try
-                    {
-                        ui = UiFactory.getInstance(type);
-                    } catch (Exception e)
-                    {
-                        logger.error(e.getStackTrace());
-                    }
-                }
-            }
-            // Check Arguments
+                    uiType = type;
+            // Add other Argument Checks
+            
             logger.info(arg);
         }
+        
+        try
+        {
+            ui = UiFactory.getInstance(uiType);
+        } catch (Exception e)
+        {
+            logger.error(e.getStackTrace());
+        }
+    }
+    
+    private static void input()
+    {
+        ui.getInput();
+    }
+    
+    private static void processing()
+    {
+    
+    }
+    
+    private static void output()
+    {
+        ui.sendOutput("Test");
     }
 }

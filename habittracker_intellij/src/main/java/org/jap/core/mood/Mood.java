4c81865c4e3e8eff2276b548ec4aa8ce67431c8b
Alexa56767 <49730170+Alexa56767@users.noreply.github.com>
2021-11-15 17:49:15 +0100

    Constructor in Mood

--- habittracker_intellij/src/main/java/org/jap/core/mood/Mood.java
+++ habittracker_intellij/src/main/java/org/jap/core/mood/Mood.java
@@ -1,5 +1,6 @@
 package org.jap.core.mood;
 
+import javafx.scene.paint.Color;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,4 +12,15 @@ public class Mood
     String name = "";
     String description = "";
     Vector2d moodLocation = new Vector2d(0,0);
+    Color color;
+
+    public Mood(String name, String description, Vector2d moodLocation) {
+        this.name = name;
+        this.description = description;
+        this.moodLocation = moodLocation;
+        calculateColor();
+    }
+
+    private void calculateColor() {
+    }
 }

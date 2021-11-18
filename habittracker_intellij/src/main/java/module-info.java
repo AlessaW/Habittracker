f7db1b97cbc2c8acea301465849ac438cc8a3dfb
Alexa56767 <49730170+Alexa56767@users.noreply.github.com>
2021-11-18 16:03:35 +0100

    Methoden in Mood

--- habittracker_intellij/src/main/java/module-info.java
+++ habittracker_intellij/src/main/java/module-info.java
@@ -5,7 +5,8 @@ module gui {
     requires vecmath;
     requires java.sql;
     requires sqlitejdbc.v056;
-    
+    requires java.desktop;
+
     opens org.jap to javafx.fxml;
     exports org.jap.examples;
     opens org.jap.examples to javafx.fxml;
--- habittracker_intellij/src/main/java/org/jap/core/mood/Mood.java
+++ habittracker_intellij/src/main/java/org/jap/core/mood/Mood.java
@@ -4,7 +4,11 @@ import javafx.scene.paint.Color;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.imageio.ImageIO;
 import javax.vecmath.Vector2d;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Mood
 {
@@ -14,13 +18,50 @@ public class Mood
     Vector2d moodLocation = new Vector2d(0,0);
     Color color;
 
-    public Mood(String name, String description, Vector2d moodLocation) {
+    public Mood(String name, String description, Vector2d moodLocation) throws IOException {
         this.name = name;
         this.description = description;
         this.moodLocation = moodLocation;
         calculateColor();
     }
 
-    private void calculateColor() {
+    public Mood(String name, String description, Vector2d moodLocation, Color color) {
+        this.name = name;
+        this.description = description;
+        this.moodLocation = moodLocation;
+        this.color = color;
+    }
+
+    /**
+     * calculating color from image coordinates
+     * @throws IOException
+     */
+    private void calculateColor() throws IOException {
+
+
+        File file = new File("imagefile.png");
+        BufferedImage image = ImageIO.read(file);
+        // Getting pixel color by position x and y
+        double imagecolor = image.getRGB((int)moodLocation.getX(), (int) moodLocation.getY());
+        //this.color = new Color(imagecolor);
+
+
+    }
+
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+
+    private void setMoodLocation(Vector2d moodLocation) {
+        this.moodLocation = moodLocation;
+    }
+
+    private void setColor(Color color) {
+        this.color = color;
     }
 }

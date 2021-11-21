5a295e1280afd89bd37d0e6d1759e7a81b6436f5
Peter <pg055@hdm-stuttgart.de>
2021-11-21 23:55:01 +0100

    habit is now interfaced

--- habittracker_intellij/src/main/java/org/jap/core/habit/HabitManager.java
+++ habittracker_intellij/src/main/java/org/jap/core/habit/HabitManager.java
@@ -3,7 +3,31 @@ package org.jap.core.habit;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Manages ALL the habits
+ */
 public class HabitManager
 {
     private static final Logger log = LogManager.getLogger(HabitManager.class);
+    
+    // Variables
+    private final List<IHabit> habits = new ArrayList<>();
+    
+    // Methods
+    /**
+     * @param habit the new Habit
+     */
+    public void add(IHabit habit){
+        habits.add(habit);
+    }
+    
+    /**
+     * @return a copy of the Habits
+     */
+    public List<IHabit> getListCopy() {
+        return new ArrayList<>(habits);
+    }
 }

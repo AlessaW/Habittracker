97905a0c4b765d032a65ea354608b3278d4b4448
Seybold Jannika <js443@hdm-stuttgart.de>
2021-12-08 16:51:34 +0100

    Projektstart: Moodtracker Ordner + dummy Classes

---
+++ moodtracker/src/main/java/org/jap/view/View.java
@@ -0,0 +1,12 @@
+package org.jap.view;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/*
+    Created by nika
+*/
+
+public class View {
+    private static final Logger log = LogManager.getLogger(View.class);
+}

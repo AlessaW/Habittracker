ed29c81a18b98f11f2d83e92327d6e55344e434c
Alexa56767 <49730170+Alexa56767@users.noreply.github.com>
2021-12-09 11:27:58 +0100

    added Mood methods and MoodManager in Model Branch

--- moodtracker/src/main/java/org/jap/model/Mood.java
+++ moodtracker/src/main/java/org/jap/model/Mood.java
@@ -9,4 +9,30 @@ import org.apache.logging.log4j.Logger;
 
 public class Mood {
     private static final Logger log = LogManager.getLogger(Mood.class);
+
+    private String name;
+    private String description;
+
+    /**
+     * @param name         The name of the mood
+     * @param description  The description of the mood
+     *
+     */
+    public Mood(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Mood(Mood mood){
+        this.name =mood.getName();
+        this.description = mood.getDescription();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }

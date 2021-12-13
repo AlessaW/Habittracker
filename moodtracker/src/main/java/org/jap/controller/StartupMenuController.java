119d85b8b518d0b7369706e019b1500641551515
Peter <pg055@hdm-stuttgart.de>
2021-12-13 21:52:03 +0100

    Reworked StartupView (renamed App) to the new SceneManager

--- moodtracker/src/main/java/org/jap/controller/StartupMenuController.java
+++ moodtracker/src/main/java/org/jap/controller/StartupMenuController.java
@@ -1,11 +1,9 @@
 package org.jap.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jap.view.SceneManager;
 
 /*
     Created by Peter
@@ -14,31 +12,14 @@ import org.apache.logging.log4j.Logger;
 /**
  * The Controller Class for the Startup View
  */
-public class StartupMenuController {
+public class StartupMenuController extends GenericController {
     private static final Logger log = LogManager.getLogger(StartupMenuController.class);
-    
-    // Variables
-    @FXML private VBox   root;          // Name im FXML File als fx:id angegeben
-    @FXML private Button btnAddMood;
-    @FXML private Button btnEditMood;
-    @FXML private Button btnViewStats;
-    @FXML private Button btnExit;
-    
-    private Stage stage;
-    
-    // Constructor
-    /**
-     * Constructor can be used to do some view initialization
-     */
-    public StartupMenuController() {
-        // Maybe Todo: view initialization
-//        root.getScene().getWindow();
-    }
     
     // Methods
     @FXML public void btnAddMoodAction() {   // Name im FXML File als Action Event angegeben
         // Todo: switch scene to createMood
         log.debug("AddMood Button Clicked");
+        getSceneManager().switchScene(SceneManager.States.CREATE_MOOD);
     }
     @FXML public void btnEditMoodAction() {   // Name im FXML File als Action Event angegeben
         // Todo: important stuff
@@ -50,11 +31,6 @@ public class StartupMenuController {
     }
     @FXML public void btnExitAction() {   // Name im FXML File als Action Event angegeben
         log.debug("Exit Button Clicked");
-        exitApplication();
-    }
-    
-    private void exitApplication() {
-        Stage stage = (Stage) root.getScene().getWindow();
-        stage.close();
+        getSceneManager().exitApplication();
     }
 }

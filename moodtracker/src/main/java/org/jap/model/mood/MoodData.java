527155e5e27f590363375261fb20d33f03da182e
Alexa56767 <49730170+Alexa56767@users.noreply.github.com>
2021-12-13 16:38:42 +0100

    MoodData Constructor and getters

--- moodtracker/src/main/java/org/jap/model/mood/MoodData.java
+++ moodtracker/src/main/java/org/jap/model/mood/MoodData.java
@@ -3,9 +3,13 @@ package org.jap.model.mood;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.time.LocalDateTime;
+
 /**
  * MoodData is the class which holds all relevant fields and methods of a Mood.
  * MoodObjects are immutable //Todo: Sachen deswegen final machen?
+ *
+ *
  * Created by Jannika
  */
 
@@ -15,9 +19,40 @@ public class MoodData {
     private String name;
     private String description;
     private int moodID;
-    private int timeStamp; //Todo: which time format is good? Sys-Time as a default value on object creation
+    private LocalDateTime timeStamp; //Todo: which time format is good? Sys-Time as a default value on object creation
     private int activityLevel; //Todo: range from -10 to 10 useful?
     private int moodValue; //Todo: range from -10 to 10 useful?
 
+    public MoodData(String name, String description, int moodID, LocalDateTime timeStamp, int activityLevel, int moodValue) {
+        this.name = name;
+        this.description = description;
+        this.moodID = moodID;
+        this.timeStamp = timeStamp;
+        this.activityLevel = activityLevel;
+        this.moodValue = moodValue;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getMoodID() {
+        return moodID;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public int getActivityLevel() {
+        return activityLevel;
+    }
+
+    public int getMoodValue() {
+        return moodValue;
+    }
 }
--- moodtracker/src/main/java/org/jap/model/mood/MoodManager.java
+++ moodtracker/src/main/java/org/jap/model/mood/MoodManager.java
@@ -3,6 +3,9 @@ package org.jap.model.mood;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * Handles the Collection of MoodData Objects
  * When a field of a MoodData object is changed, it creates a new MoodData Object out of the new input and the data from the previous object
@@ -12,4 +15,6 @@ import org.apache.logging.log4j.Logger;
 
 public class MoodManager {
     private static final Logger log = LogManager.getLogger(MoodManager.class);
+
+    Collection moods = new ArrayList<MoodData>();
 }

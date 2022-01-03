f7078f99d350df6c1efdb53a25d03075faac6246
Alessa Wiesner <aw184@hdm-stuttgart.de>
2022-01-03 15:58:09 +0100

    dataManager in MoodManager

--- moodtracker/src/main/java/org/jap/model/mood/MoodManager.java
+++ moodtracker/src/main/java/org/jap/model/mood/MoodManager.java
@@ -20,6 +20,8 @@ import java.util.List;
 public class MoodManager {
     private static final Logger log = LogManager.getLogger(MoodManager.class);
 
+    private DataManager dataManager;
+
     private List<MoodData> moods;
 
     // todo: maybe add a init method for initiation of static variable for better control
@@ -27,7 +29,8 @@ public class MoodManager {
 
     private MoodManager() {
         this.moods = new ArrayList<MoodData>();
-
+        this.dataManager = new DataManager();
+        moods = dataManager.loadMoods();
     }
 
     public static MoodManager getInstance(){
@@ -58,6 +61,7 @@ public class MoodManager {
      */
     public void addMood(MoodData mood){
         moods.add(mood);
+        dataManager.saveMood(mood);
     }
 
 
@@ -65,30 +69,43 @@ public class MoodManager {
         MoodData newMood = new MoodData(mood.getMoodID(), name,mood.getDescription(),mood.getTimeStamp(), mood.getActivityLevel(), mood.getMoodValue());
         deleteMood(mood);
         moods.add(newMood);
+        dataManager.saveMood(newMood);
     }
 
     public void changeDescription(MoodData mood, String description){
         MoodData newMood = new MoodData(mood.getMoodID(), mood.getName(), description,mood.getTimeStamp(), mood.getActivityLevel(), mood.getMoodValue());
         deleteMood(mood);
         moods.add(newMood);
+        dataManager.saveMood(newMood);
     }
     public void changeTimeStamp(MoodData mood, LocalDateTime timeStamp){
         MoodData newMood = new MoodData(mood.getMoodID(), mood.getName(),mood.getDescription(), timeStamp, mood.getActivityLevel(), mood.getMoodValue());
         deleteMood(mood);
         moods.add(newMood);
+        dataManager.saveMood(newMood);
     }
     public void changeActivityLevel(MoodData mood, int activityLevel){
         MoodData newMood = new MoodData(mood.getMoodID(), mood.getName(),mood.getDescription(),mood.getTimeStamp(), activityLevel, mood.getMoodValue());
         deleteMood(mood);
         moods.add(newMood);
+        dataManager.saveMood(newMood);
     }
     public void changeMoodValue(MoodData mood, int moodValue){
         MoodData newMood = new MoodData(mood.getMoodID(), mood.getName(),mood.getDescription(),mood.getTimeStamp(), mood.getActivityLevel(), moodValue);
         deleteMood(mood);
         moods.add(newMood);
+        dataManager.saveMood(newMood);
     }
 
     public void deleteMood(MoodData mood){
         moods.remove(mood);
+        dataManager.deleteMood(mood);
+    }
+
+    /**
+     * closes the dataManager
+     */
+    public void close(){
+        dataManager.close();
     }
 }

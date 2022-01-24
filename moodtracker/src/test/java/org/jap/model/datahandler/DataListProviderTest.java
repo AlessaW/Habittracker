34748255072012d994ab392568af7018bde19ff3
Seybold Jannika <js443@hdm-stuttgart.de>
2022-01-24 16:51:16 +0100

    Start Test für DataListProvider

--- moodtracker/src/test/java/org/jap/model/datahandler/DataListProviderTest.java
+++ moodtracker/src/test/java/org/jap/model/datahandler/DataListProviderTest.java
@@ -1,4 +1,5 @@
 package org.jap.model.datahandler;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,4 +9,13 @@ import org.apache.logging.log4j.Logger;
 
 public class DataListProviderTest {
     private static final Logger log = LogManager.getLogger(DataListProviderTest.class);
+
+    //give "fake" moodDatalist
+
+    //Test weekly
+    //single days
+    //multiple data entries on one day
+    //fail: konnte nich ausgeführt werden
+    //keine Data vorhanden
+
 }

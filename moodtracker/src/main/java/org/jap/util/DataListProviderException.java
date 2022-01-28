4b3590e1cec152c2a330f755d8b7d3728ce40470
Seybold Jannika <js443@hdm-stuttgart.de>
2022-01-28 23:08:24 +0100

    DataListProviderException

--- moodtracker/src/main/java/org/jap/util/DataListProviderException.java
+++ moodtracker/src/main/java/org/jap/util/DataListProviderException.java
@@ -1,4 +1,5 @@
 package org.jap.util;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -6,6 +7,9 @@ import org.apache.logging.log4j.Logger;
     Created by nika
 */
 
-public class DataListProviderException {
+public class DataListProviderException extends Throwable{
     private static final Logger log = LogManager.getLogger(DataListProviderException.class);
-}
+        public DataListProviderException(String message) {
+            super(message);
+        }
+    }

c0129f024491208563563c66937844d706fa0ef2
Seybold Jannika <js443@hdm-stuttgart.de>
2022-01-28 23:07:38 +0100

    CustomException added for DataListProvider

---
+++ moodtracker/src/main/java/org/jap/util/DataListProviderException.java
@@ -0,0 +1,11 @@
+package org.jap.util;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/*
+    Created by nika
+*/
+
+public class DataListProviderException {
+    private static final Logger log = LogManager.getLogger(DataListProviderException.class);
+}
